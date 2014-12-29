8cdbe5607546cb5b50573a5f397fe6a228f5217c
Tim Routowicz <troutowicz@gmail.com>
2014-12-28 23:42:18 -0500

    Add files

---
+++ test/decide.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+
+  var yesnowtf = require('../index.js'),
+      chai = require('chai'),
+      chaiAsPromised = require('chai-as-promised'),
+      expect = chai.expect;
+
+  chai.use(chaiAsPromised);
+
+  describe('#decide()', function() {
+    it('should return a random decision if no parameters are passed', function(done) {
+      expect(yesnowtf.decide())
+        .to.be.fulfilled
+        .and.notify(done);
+    });
+
+    it('should return a yes decision if parameter \'force\' equals \'yes\'', function(done) {
+      var decision = 'yes';
+
+      expect(yesnowtf.decide(decision))
+        .to.eventually.have.property('answer', 'yes')
+        .and.notify(done);
+    });
+
+    it('should return a no decision if parameter \'force\' equals \'no\'', function(done) {
+      var decision = 'no';
+
+      expect(yesnowtf.decide(decision))
+        .to.eventually.have.property('answer', 'no')
+        .and.notify(done);
+    });
+
+    it('should return a maybe decision if parameter \'force\' equals \'maybe\'', function(done) {
+      var decision = 'maybe';
+
+      expect(yesnowtf.decide(decision))
+        .to.eventually.have.property('answer', 'maybe')
+        .and.notify(done);
+    });
+  });
+}());
