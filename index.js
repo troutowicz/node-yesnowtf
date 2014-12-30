17f89f7aead61e4a225dab206f8bdc2c9fa20815
Tim Routowicz <troutowicz@gmail.com>
2014-12-29 19:20:21 -0500

    Do not use a SEF, miscellaneous adjustments

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-(function() {
-  'use strict';
+'use strict';
 
-  var http = require('http'),
-      Q = require('q'),
-      API_ENDPOINT = 'http://yesno.wtf/api';
+var http = require('http'),
+    Q = require('q'),
+    API_ENDPOINT = 'http://yesno.wtf/api';
 
-  function decide(decision, cb) {
-    var deferred = Q.defer(),
-        url = API_ENDPOINT + (decision ? '?force=' + decision : '');
+function decide(decision, cb) {
+  var deferred = Q.defer(),
+      url = API_ENDPOINT + (decision ? '?force=' + decision : '');
 
-    http.get(url, function (res) {
-      res.setEncoding('utf8');
-
-      var data = '';
-      res.on('data', function (chunk) {
-        data += chunk;
-      });
-
-      res.on('end', function () {
-        Q.fcall(function () {
-          return JSON.parse(data);
-        })
-        .then(function (result) {
-          deferred.resolve(result);
-        }, function (exception) {
-          deferred.reject(new Error('JSON parse failed'));
-        });
-      });
-    }).on('error', function(err) {
-      deferred.reject(err);
-    });
-
-    return deferred.promise.nodeify(cb);
+  if (cb && typeof cb !== 'function') {
+    throw new TypeError(typeof cb + ' is not a function');
   }
 
-  module.exports = {
-    decide: decide
-  };
-}());
+  http.get(url, function (res) {
+    res.setEncoding('utf8');
+
+    var data = '';
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+
+    res.on('end', function () {
+      try {
+        var result = JSON.parse(data);
+        deferred.resolve(result);
+      } catch (e) {
+        deferred.reject(new Error('JSON parse failed'));
+      }
+    });
+  }).on('error', function(err) {
+    deferred.reject(err);
+  });
+
+  return deferred.promise.nodeify(cb);
+}
+
+module.exports.decide = decide;
