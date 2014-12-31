548ed525542cec2d07dbe856898af5ef1ffad3a3
Tim Routowicz <troutowicz@gmail.com>
2014-12-30 22:12:19 -0500

    Update tests

--- test/decide.js
+++ test/decide.js
@@ -4,55 +4,105 @@ var yesnowtf = require('../index.js'),
     chai = require('chai'),
     chaiAsPromised = require('chai-as-promised'),
     chaiProperties = require('chai-properties'),
-    expect = chai.expect,
-    should = chai.should();
+    expect = chai.expect;
 
 chai.use(chaiAsPromised);
 chai.use(chaiProperties)
 
 describe('#decide()', function() {
-  it('should return a random decision object if no decision parameter is passed', function (done) {
-    expect(yesnowtf.decide())
-      .to.eventually.be.an('object')
-      .and.notify(done);
+  describe('using promises', function() {
+    it('should return a random decision object if no decision parameter is passed', function () {
+      return expect(yesnowtf.decide())
+        .to.eventually.be.an('object')
+        .and.have.property('forced', false);
+    });
+
+    it('should return a yes decision object if decision parameter equals \'yes\'', function () {
+      var decision = 'yes';
+
+      return expect(yesnowtf.decide(decision))
+        .to.eventually.be.an('object')
+        .then(function (data) {
+          expect(data).to.have.properties({answer: 'yes', forced: true});
+        });
+    });
+
+    it('should return a no decision object if decision parameter equals \'no\'', function () {
+      var decision = 'no';
+
+      return expect(yesnowtf.decide(decision))
+        .to.eventually.be.an('object')
+        .then(function (data) {
+          expect(data).to.have.properties({answer: 'no', forced: true});
+        });
+    });
+
+    it('should return a maybe decision object if decision parameter equals \'maybe\'', function () {
+      var decision = 'maybe';
+
+      return expect(yesnowtf.decide(decision))
+        .to.eventually.be.an('object')
+        .then(function (data) {
+          expect(data).to.have.properties({answer: 'maybe', forced: true});
+        });
+    });
   });
 
-  it('should return a yes decision object if decision parameter equals \'yes\'', function (done) {
-    var decision = 'yes';
+  describe('using callbacks', function() {
+    it('should return a random decision object if no decision parameter is passed', function (done) {
+      var decide;
 
-    expect(yesnowtf.decide(decision))
-      .to.eventually.be.an('object')
-      .then(function (data) {
-        expect(data).to.have.properties({answer: 'yes', forced: true});
-      }).should.notify(done);
-  });
+      yesnowtf.decide(decide, function(err, data) {
+        if (err) return done(err);
 
-  it('should return a no decision object if decision parameter equals \'no\'', function (done) {
-    var decision = 'no';
+        expect(data)
+          .to.be.an('object')
+          .and.have.property('forced', false);
 
-    expect(yesnowtf.decide(decision))
-      .to.eventually.be.an('object')
-      .then(function (data) {
-        expect(data).to.have.properties({answer: 'no', forced: true});
-      }).should.notify(done);
-  });
+        done();
+      });
+    });
 
-  it('should return a maybe decision object if decision parameter equals \'maybe\'', function (done) {
-    var decision = 'maybe';
+    it('should return a yes decision object if decision parameter equals \'yes\'', function (done) {
+      var decision = 'yes';
 
-    expect(yesnowtf.decide(decision))
-      .to.eventually.be.an('object')
-      .then(function (data) {
-        expect(data).to.have.properties({answer: 'maybe', forced: true});
-      }).should.notify(done);
-  });
+      yesnowtf.decide(decision, function(err, data) {
+        if (err) return done(err);
 
-  it('should throw type error if callback parameter is set and not a function', function () {
-    var callback = 'string';
-    var err = typeof callback + ' is not a function';
+        expect(data)
+          .to.be.an('object')
+          .and.have.properties({answer: 'yes', forced: true});
 
-    expect(function () {
-      yesnowtf.decide(null, callback);
-    }).to.throw(err);
+        done();
+      });
+    });
+
+    it('should return a no decision object if decision parameter equals \'no\'', function (done) {
+      var decision = 'no';
+
+      yesnowtf.decide(decision, function(err, data) {
+        if (err) return done(err);
+
+        expect(data)
+          .to.be.an('object')
+          .and.have.properties({answer: 'no', forced: true});
+
+        done();
+      });
+    });
+
+    it('should return a maybe decision object if decision parameter equals \'maybe\'', function (done) {
+      var decision = 'maybe';
+
+      yesnowtf.decide(decision, function(err, data) {
+        if (err) return done(err);
+
+        expect(data)
+          .to.be.an('object')
+          .and.have.properties({answer: 'maybe', forced: true});
+
+        done();
+      });
+    });
   });
 });
