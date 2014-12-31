0be5a4cbf8fd252de8f0335582afd65158d28fb9
Tim Routowicz <troutowicz@gmail.com>
2014-12-30 22:11:46 -0500

    Use q-io/http for api requests

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 'use strict';
 
-var http = require('http'),
-    Q = require('q'),
-    API_ENDPOINT = 'http://yesno.wtf/api';
+var http = require('q-io/http'),
+    q = require('q'),
+    apiEndpoint = 'http://yesno.wtf/api';
 
 function decide(decision, cb) {
-  var deferred = Q.defer(),
-      url = API_ENDPOINT + (decision ? '?force=' + decision : '');
+  var deferred = q.defer(),
+      url = apiEndpoint + (decision ? '?force=' + decision : '');
 
-  if (cb && typeof cb !== 'function') {
-    throw new TypeError(typeof cb + ' is not a function');
-  }
-
-  http.get(url, function (res) {
-    res.setEncoding('utf8');
-
-    var data = '';
-    res.on('data', function (chunk) {
-      data += chunk;
-    });
-
-    res.on('end', function () {
+  http.read(url).then(
+    function (data) {
       try {
         var result = JSON.parse(data);
         deferred.resolve(result);
       } catch (e) {
         deferred.reject(new Error('JSON parse failed'));
       }
-    });
-  }).on('error', function(err) {
-    deferred.reject(err);
-  });
+    },
+    function (err) {
+      deferred.reject(err);
+    }
+  );
 
   return deferred.promise.nodeify(cb);
 }
